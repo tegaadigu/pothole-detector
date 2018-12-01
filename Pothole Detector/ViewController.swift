16473ec46f75f812c9fb99760e27b45ffc08b5a5
Tega Adigu <tega.adigu@gmail.com>
2018-12-01 12:12:34 -0600

    Initial Commit

---
+++ Pothole Detector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pothole Detector
+//
+//  Created by Tega Adigu on 01/12/2018.
+//  Copyright © 2018 Tega Adigu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
